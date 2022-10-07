328cb1e6bfd92ab6cf61228d617cd020404d193a
Alexander <a.schmidt.2002@gmx.de>
2022-10-07 12:54:36 +0200

    added short way to delete region

--- frontend/src/components/AdminRegions.jsx
+++ frontend/src/components/AdminRegions.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import axios from "axios";
 import { useKeycloak } from "@react-keycloak-fork/web";
-import { Table, ActionIcon } from '@mantine/core';
-import RegionView from "../components/RegionView";
+import { Table, ActionIcon, Box, Text } from '@mantine/core';
+import { RegionView } from "../components/RegionView";
 import { BiEdit } from 'react-icons/bi';
+import { MdDelete } from 'react-icons/md';
+import { useModals } from "@mantine/modals";
 
 
 const AdminRegions = () => {
     const { keycloak } = useKeycloak();
+    const modals = useModals();
     const [regions, setRegions] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [openRegionView, setOpenRegionView] = React.useState(false);
@@ -20,6 +23,34 @@ const AdminRegions = () => {
 
     useEffect(() => { getRegions() }, []);
 
+
+    const showDeleteConfirmation = (region) => {
+        modals.openConfirmModal({
+            title: 'Delete this region?',
+            centered: true,
+            children: (
+                <Text size="sm">
+                    Are you sure you want to delete this region? <b>This process is irreversible.</b>
+                </Text>
+            ),
+            labels: { confirm: 'Delete region', cancel: "No don't delete it" },
+            confirmProps: { color: 'red' },
+            onConfirm: () => {
+                deleteRegion(region.id);
+            },
+        });
+    }
+
+    const deleteRegion = async (id) => {
+        await axios.delete(`/api/v1/region/${id}`, { headers: { authorization: "Bearer " + keycloak.token } });
+        showNotification({
+            title: 'Region deleted!',
+            message: 'This region has been deleted.',
+            color: "red"
+        })
+    }
+
+
     const getRegions = async () => {
         const { data } = await axios.get(`api/v1/region/all`, { headers: { authorization: "Bearer " + keycloak.token } });
         setRegions(data);
@@ -31,10 +62,16 @@ const AdminRegions = () => {
             <td>{element.city}</td>
             <td>{element.area}</td>
             <td>{element.username}</td>
-            <td>
-                <ActionIcon onClick={() => editRegion(element)}>
-                    <BiEdit />
-                </ActionIcon> </td>
+            <td >
+                <Box sx={{ display: "flex" }}>
+                    <ActionIcon onClick={() => editRegion(element)}>
+                        <BiEdit />
+                    </ActionIcon>
+                    <ActionIcon onClick={() => showDeleteConfirmation(element)}>
+                        <MdDelete />
+                    </ActionIcon>
+                </Box>
+            </td>
         </tr>
     ));
 
