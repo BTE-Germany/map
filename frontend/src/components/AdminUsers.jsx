25756a9192a9514fb048a1cf6f7e1e2824233147
Alexander <a.schmidt.2002@gmx.de>
2022-10-09 17:55:06 +0200

    formatter warfare

--- frontend/src/components/AdminUsers.jsx
+++ frontend/src/components/AdminUsers.jsx
@@ -6,87 +6,112 @@
  + This project is released under the MIT license.                            +
  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-import React, { useEffect } from 'react';
+import React, {useEffect} from "react";
 import axios from "axios";
-import { useKeycloak } from "@react-keycloak-fork/web";
-import { ActionIcon, Badge, Box, Loader, Table, Tooltip } from "@mantine/core";
-import { BsFileEarmarkLock2 } from "react-icons/bs";
-import { BiLockOpen } from "react-icons/bi";
+import {useKeycloak} from "@react-keycloak-fork/web";
+import {ActionIcon, Badge, Box, Loader, Table, Tooltip} from "@mantine/core";
+import {BsFileEarmarkLock2} from "react-icons/bs";
+import {BiLockOpen} from "react-icons/bi";
 
-
-const AdminUsers = props => {
+const AdminUsers = (props) => {
     const [users, setUsers] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
-    const { keycloak } = useKeycloak();
+    const {keycloak} = useKeycloak();
 
     useEffect(() => {
         getUsers();
     }, []);
 
     const getUsers = async () => {
-        const { data } = await axios.get(`api/v1/admin/user/@list`, { headers: { authorization: "Bearer " + keycloak.token } });
+        const {data} = await axios.get(`api/v1/admin/user/@list`, {
+            headers: {authorization: "Bearer " + keycloak.token},
+        });
         setUsers(data);
         setIsLoading(false);
-    }
+    };
     const lockUser = async (user) => {
-        await axios.post(`api/v1/admin/user/@lock`, { userId: user }, { headers: { authorization: "Bearer " + keycloak.token } });
+        await axios.post(
+            `api/v1/admin/user/@lock`,
+            {userId: user},
+            {headers: {authorization: "Bearer " + keycloak.token}}
+        );
         getUsers();
-    }
+    };
 
     const unlockUser = async (user) => {
-        await axios.post(`api/v1/admin/user/@unlock`, { userId: user }, { headers: { authorization: "Bearer " + keycloak.token } });
+        await axios.post(
+            `api/v1/admin/user/@unlock`,
+            {userId: user},
+            {headers: {authorization: "Bearer " + keycloak.token}}
+        );
         getUsers();
-    }
+    };
 
     const rows = users.map((element) => (
         <tr key={element.id}>
             <td>{element.username}</td>
             <td>{element.email}</td>
-            <td>{element.emailVerified && <Badge color="lime">Verifizierte Mail</Badge>} {!element.totp &&
-                <Badge color="yellow">Kein TOTP</Badge>} {!element.enabled && <Badge color="red">Gesperrt</Badge>}</td>
             <td>
-                <Box sx={(theme) => {
-                    return {
-                        display: "flex",
-                        gap: theme.spacing[2],
-                    }
-                }}>
-                    {
-                        element.enabled ? <Tooltip label="Sperren">
-                            <ActionIcon color="red" variant="light" onClick={() => lockUser(element.id)}>
+                {element.emailVerified && (
+                    <Badge color="lime">Verifizierte Mail</Badge>
+                )}{" "}
+                {!element.totp && <Badge color="yellow">Kein TOTP</Badge>}{" "}
+                {!element.enabled && <Badge color="red">Gesperrt</Badge>}
+            </td>
+            <td>
+                <Box
+                    sx={(theme) => {
+                        return {
+                            display: "flex",
+                            gap: theme.spacing[2],
+                        };
+                    }}
+                >
+                    {element.enabled ? (
+                        <Tooltip label="Sperren">
+                            <ActionIcon
+                                color="red"
+                                variant="light"
+                                onClick={() => lockUser(element.id)}
+                            >
                                 <BsFileEarmarkLock2 />
                             </ActionIcon>
-                        </Tooltip> : <Tooltip label="Entsperren">
-                            <ActionIcon color="green" variant="light" onClick={() => unlockUser(element.id)}>
+                        </Tooltip>
+                    ) : (
+                        <Tooltip label="Entsperren">
+                            <ActionIcon
+                                color="green"
+                                variant="light"
+                                onClick={() => unlockUser(element.id)}
+                            >
                                 <BiLockOpen />
                             </ActionIcon>
                         </Tooltip>
-                    }
-
+                    )}
                 </Box>
             </td>
         </tr>
     ));
 
-
     return (
         <div>
-            {
-                isLoading ? <Loader /> :
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Benutzername</th>
-                                <th>E-Mail Adresse</th>
-                                <th>Tags</th>
-                                <th>Aktionen</th>
-                            </tr>
-                        </thead>
-                        <tbody>{rows}</tbody>
-                    </Table>
-            }
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Benutzername</th>
+                            <th>E-Mail Adresse</th>
+                            <th>Tags</th>
+                            <th>Aktionen</th>
+                        </tr>
+                    </thead>
+                    <tbody>{rows}</tbody>
+                </Table>
+            )}
         </div>
     );
-}
+};
 
-export default AdminUsers
+export default AdminUsers;
