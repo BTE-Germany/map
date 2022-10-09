9a9c500674642a041da9f5dac246b1934737188b
Robin Ferch <robin@ferch-mail.de>
2022-10-09 16:28:54 +0200

    update to mantine 5.5.4 to prepare for the image view

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -7,47 +7,23 @@
  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 import React from 'react';
-import {createStyles, Group, Switch, useMantineColorScheme} from "@mantine/core";
+import {createStyles, Group, Switch, useMantineColorScheme, useMantineTheme} from "@mantine/core";
 import {BiSun, BiMoon} from "react-icons/bi";
-
-const useStyles = createStyles((theme) => ({
-    root: {
-        position: 'relative',
-        '& *': {
-            cursor: 'pointer',
-        },
-    },
-
-    icon: {
-        pointerEvents: 'none',
-        position: 'absolute',
-        zIndex: 1,
-        top: 3,
-    },
-
-    iconLight: {
-        left: 4,
-        color: theme.white,
-    },
-
-    iconDark: {
-        right: 4,
-        color: theme.colors.gray[6],
-    },
-}));
 
 const ThemeToggle = props => {
     const {colorScheme, toggleColorScheme} = useMantineColorScheme();
-    const {classes, cx} = useStyles();
-
+    const theme = useMantineTheme();
     return (
         <Group position="center" my={30}>
-            <div className={classes.root}>
-                <BiSun className={cx(classes.icon, classes.iconLight)} size={18}/>
-                <BiMoon className={cx(classes.icon, classes.iconDark)} size={18}/>
-                <Switch checked={colorScheme === 'dark'} onChange={() => toggleColorScheme()} size="md"/>
-            </div>
+            <Switch
+                checked={colorScheme === 'dark'}
+                onChange={() => toggleColorScheme()}
+                size="md"
+                onLabel={<BiSun color={theme.white} size={20} stroke={1.5}/>}
+                offLabel={<BiMoon color={theme.colors.gray[6]} size={20} stroke={1.5}/>}
+            />
         </Group>
+
     );
 }
 
