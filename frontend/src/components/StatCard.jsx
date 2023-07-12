c7059e82dbdc4eec851e01cce77cd01466824136
Alexander <a.schmidt.2002@gmx.de>
2023-07-12 18:57:59 +0200

    updated StatCard to incorporate new Features

--- frontend/src/components/StatCard.jsx
+++ frontend/src/components/StatCard.jsx
@@ -32,30 +32,39 @@ const useStyles = createStyles((theme) => ({
     },
 }));
 
-const StatCard = ({title, Icon, innerImage, value, subtitle, noBigValue, editable, id}) => {
+const StatCard = ({title, Icon, innerImage, value, subtitle, noBigValue, editable, id, additionalElement = null, showAdditionalElement = true, visible = true, skinny = false}) => {
     const {classes} = useStyles();
-
+    if (!visible)
+        return null;
     return (
-        <Paper withBorder p="md" radius="md" shadow="md" sx={{width: "100%"}}>
-            <Group position="apart">
+        <Paper withBorder p={skinny ? 0 : "md"} radius="md" shadow="md" sx={{width: "100%"}}>
+            <Group position="apart" m={skinny ? "md" : 0} mb={0}>
                 <Text size="xs" color="dimmed" className={classes.title}>
                     {title}
                 </Text>
-                <Icon className={classes.icon} size={22}/>
+                <Icon className={classes.icon} size={22} />
             </Group>
 
-            <Group align="flex-end" spacing="xs" mt={15}>
-                <Box sx={{display: "flex", alignItems: "center"}} style={{width: "100%"}}>
-                    {innerImage ?
-                        <Image src={innerImage} alt="" radius={"md"} style={{width: 64, marginRight: 10}}/> :
-                        null
-                    }
-                    {editable ?
-                        <TextInput id={id} defaultValue={value} style={{width: "100%"}}/> :
-                        !noBigValue ?
-                            <Title ml={"md"} order={3} className={classes.value}>{value}</Title> : <>{value}</>
-                    }
-                </Box>
+            <Group align="flex-end" spacing="xs" mt={skinny ? 0 : 15} width="100%">
+                {value != null ?
+                    <Box sx={{display: "flex", alignItems: "center"}} style={{width: "100%"}}>
+                        {innerImage ?
+                            <Image src={innerImage} alt="" radius={"md"} style={{width: 64, marginRight: 10}} /> :
+                            null
+                        }
+                        {editable ?
+                            <TextInput id={id} defaultValue={value} style={{width: "100%"}} /> :
+                            !noBigValue ?
+                                <Title ml={"md"} order={3} className={classes.value}>{value}</Title> : value
+                        }
+                    </Box>
+                    : null
+                }
+                {showAdditionalElement && additionalElement ?
+                    <Box mr={skinny ? "md" : 0} style={{width: "100%"}}>
+                        {additionalElement}
+                    </Box> : null
+                }
             </Group>
 
             <Text size="xs" color="dimmed" mt={7}>
